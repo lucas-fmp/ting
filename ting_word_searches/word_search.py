ef8bfcc9313250ff25ac86c98b1ae977c8c0430c
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-04-09 19:58:38 -0300

    feat - requirement 8 - implement function to search words in the queue and show the content

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,4 +16,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        lines = []
+        for num_line, line in enumerate(file["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                lines.append({
+                    "linha": num_line,
+                    "conteudo": line
+                })
+        if lines:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines
+            })
+    return result
